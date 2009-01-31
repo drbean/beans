a74d156f2b25a009749aef07a8f7420e287eb248
drbean <drbean@a817cad4-e040-dd11-9b0b-000bcdefe56b>
2009-01-31 13:19:40 +0000

    r96@localhost:  greg | 2009-01-30 22:40:39 +0900  Mpve request and list methods from Homework.pm to Root.pm

--- beans/trunk/lib/beans/Controller/Root.pm
+++ beans/trunk/lib/beans/Controller/Root.pm
@@ -58,6 +58,46 @@ sub classwork : Local {
 	my ($self, $c) = @_;
 }
 
+=head2 homework
+
+Seek listing of homework scores for one player.
+
+=cut
+
+sub homework : Local {
+	my ($self, $c) = @_;
+}
+
+=head2 homework_listing
+
+Calculate homework score for one player using Moose homework script.
+
+=cut
+
+sub homework_listing : Local {
+	my ($self, $c) = @_;
+	my $params = $c->request->params;
+	my $leagueId = $params->{league};
+	my $playerName = $params->{player};
+	my $playerId = $params->{id};
+	my $league = League->new( leagueId => "/home/drbean/class/$leagueId" );
+	if ( $league and $league->is_member($playerId) )
+	{
+		my $player = Player->new( league => $league, id => $playerId );
+		if ( $playerName eq $player->name ) {
+			my $rounds = $league->rounds;
+			my $grades = $player->grades;
+			$c->stash->{league} = $leagueId;
+			$c->stash->{player} = $playerName;
+			$c->stash->{id} = $playerId;
+			$c->stash->{weeks} = [ map { { name => $rounds->[$_],
+				score => $grades->[$_] } } 0..$#$grades ];
+			$c->stash->{total} = $player->total;
+			$c->stash->{percent} = $player->percent;
+		}
+	}
+}
+
 =head1 AUTHOR
 
 Dr Bean
