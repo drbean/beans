9653c9067d8c2e69e77102da5b940d290a225e31
drbean <drbean@a817cad4-e040-dd11-9b0b-000bcdefe56b>
2009-03-30 13:56:35 +0000

    r1193@localhost:  drbean | 2009-03-23 18:03:55 +0800  Remove right test

--- beans/trunk/lib/Bean.pm
+++ beans/trunk/lib/Bean.pm
@@ -167,7 +167,7 @@ sub _build_allfiles {
 	my $league = $self->leagueId;
 	my $series = $self->series;
 	my $files = [ map { grep m|/(\d+)\.yaml$|, glob "$league/$_/*" } @$series ];
-	die "@$files" unless @$files;
+	# die "@$files" unless @$files;
 	return $files;
 
 }
