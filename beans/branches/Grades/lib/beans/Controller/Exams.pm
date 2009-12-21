f381f2c69b119e40ae8f055ec38146f07ec32947
drbean <drbean@a817cad4-e040-dd11-9b0b-000bcdefe56b>
2009-12-21 09:13:57 +0000

    r2596@e_learning:  drbean | 2009-12-09 16:25:04 +0800  sts=4, for raw action

--- beans/branches/Grades/lib/beans/Controller/Exams.pm
+++ beans/branches/Grades/lib/beans/Controller/Exams.pm
@@ -71,37 +71,44 @@ Show exam tallies that allowed allocation of exam grade.
 =cut
 
 sub raw : Local {
-	my ($self, $c) = @_;
-	my $params = $c->request->params;
-	my $leagueId = $params->{league} || $c->request->args->[0];
-	my $playerId = $params->{id} || $c->request->args->[1];
-	my $playerName = $params->{player} || $c->request->args->[2];
-	my $exam =                      $c->request->args->[3];
-	my $league = League->new( id => "/home/drbean/class/$leagueId" );
-	my $work = Grades->new( league => $league );
-	if ( $league and $league->is_member($playerId) )
-	{
-		my $player = Player->new( league => $league, id => $playerId );
-		if ( $playerName eq $player->name ) {
-			$c->stash->{league} = $leagueId;
-			$c->stash->{player} = $playerName;
-			$c->stash->{id} = $playerId;
-			$c->stash->{examId} = $exam;
-			my $group = $work->name2examGroup($exam, $playerName)->[0];
-			my $member = $work->examGroupMembers($exam, $group);
-			my $rawscores = $work->rawExamscores($exam, $group);
-			my @scores = map { { role => $_, name => $member->{$_},
-					score => $rawscores->{$_} }
-					} sort keys %$rawscores;
-			$c->stash->{scores} = \@scores;
-			my $total = sum ( map { $_->{score} } @scores );
-			$c->stash->{total} = $total;
-			my $grade = $work->examResultHash->{$playerId}->{$exam};
-			$c->stash->{grade} = $grade;
-			$c->stash->{group} = $group;
-			$c->stash->{template} = 'rawexam.tt2';
-		}
-	}
+    my ( $self, $c ) = @_;
+    my $params     = $c->request->params;
+    my $leagueId   = $params->{league} || $c->request->args->[0];
+    my $playerId   = $params->{id} || $c->request->args->[1];
+    my $playerName = $params->{player} || $c->request->args->[2];
+    my $exam       = $c->request->args->[3];
+    my $league     = League->new( id => "/home/drbean/class/$leagueId" );
+    my $work       = Grades->new( league => $league );
+    if ( $league and $league->is_member($playerId) ) {
+        my $player = Player->new( league => $league, id => $playerId );
+        if ( $playerName eq $player->name ) {
+            $c->stash->{league} = $leagueId;
+            $c->stash->{player} = $playerName;
+            $c->stash->{id}     = $playerId;
+            $c->stash->{examId} = $exam;
+            my $group = $work->name2examGroup( $exam, $playerName )->[0];
+            my $quiz = $group->quiz( $exam, $group );
+            my $responses = $group->responses( $exam, $group );
+            my $member = $work->examGroupMembers( $exam, $group );
+            my $rawscores = $work->rawExamscores( $exam, $group );
+            my $role = $work->id2examGroupRole( $exam, $group );
+            my @scores = map {
+                {
+                    role  => $role->{$_},
+                    name  => $member->{$_},
+                    score => $rawscores->{$_}
+                }
+              } sort { $role->{$a} cmp $role->{$b} }
+              keys %$rawscores;
+            $c->stash->{scores} = \@scores;
+            my $total = sum( map { $_->{score} } @scores );
+            $c->stash->{total} = $total;
+            my $grade = $work->examResultHash->{$playerId}->{$exam};
+            $c->stash->{grade}    = $grade;
+            $c->stash->{group}    = $group;
+            $c->stash->{template} = 'rawexam.tt2';
+        }
+    }
 }
 
 =head1 AUTHOR
