0bda4360d79b5346fa67163b93ad8146bff3146a
drbean <drbean@a817cad4-e040-dd11-9b0b-000bcdefe56b>
2009-12-21 09:14:54 +0000

    r2613@e_learning:  drbean | 2009-12-19 17:33:43 +0800  raw action uses bye, late, unpaired, transfer Nonentity class for its name value

--- beans/branches/Grades/lib/beans/Controller/Classwork.pm
+++ beans/branches/Grades/lib/beans/Controller/Classwork.pm
@@ -80,56 +80,65 @@ Show classwork tallies that allowed allocation of homework grade.
 =cut
 
 sub raw : Local {
-	my ($self, $c) = @_;
-	my $params = $c->request->params;
-	my $leagueId = $params->{league} || $c->request->args->[0];
-	my $playerId = $params->{id} || $c->request->args->[1];
-	my $playerName = $params->{player} || $c->request->args->[2];
-	my $round =                      $c->request->args->[3];
-	my $league = League->new( id => "/home/drbean/class/$leagueId" );
-	my $work = Grades->new( league => $league );
-	if ( $league and $league->is_member($playerId) )
-	{
-		my $player = Player->new( league => $league, id => $playerId );
-		if ( $playerName eq $player->name ) {
-			my $approach = $league->approach;
-			$c->stash->{league} = $leagueId;
-			$c->stash->{player} = $playerName;
-			$c->stash->{id} = $playerId;
-			$c->stash->{round} = $round;
-			$c->stash->{approach} = $approach;
-			my $exercise;
-			if ( $approach eq "compwork" ) {
-				my $qns = $work->correct( $round );
-				my $correct = $qns->{$playerId};
-				my $others = $work->opponents( $round );
-				my $other = Player->new( league => $league,
-					id => $others->{$playerId} );
-				my $otherid = $other->id;
-				my $othercorrect = $qns->{$otherid};
-				$exercise = {
-					correct => $correct,
-					otherid => $otherid,
-					othername => $other->name,
-					othercorrect => $othercorrect};
-			}
-			else {
-				my $session = $work->week2session($round);
-				my $can = $work->name2beancan( 
-					$round, $playerName );
-				my $members = $work->beancans($session)->{$can};
-				my $merits = $work->merits( $round )->{$can};
-				my $demerits = $work->demerits($round)->{$can};
-				$exercise = {
-					beancan => $can,
-					members => $members,
-					merits => $merits,
-					demerits => $demerits, };
-			}
-			$c->stash->{exercise} = $exercise;
-			$c->stash->{template} = 'rawclasswork.tt2';
+    my ( $self, $c ) = @_;
+    my $params     = $c->request->params;
+    my $leagueId   = $params->{league} || $c->request->args->[0];
+    my $playerId   = $params->{id} || $c->request->args->[1];
+    my $playerName = $params->{player} || $c->request->args->[2];
+    my $round      = $c->request->args->[3];
+    my $league     = League->new( id => "/home/drbean/class/$leagueId" );
+    my $work       = Grades->new( league => $league );
+    if ( $league and $league->is_member($playerId) ) {
+        my $player = Player->new( league => $league, id => $playerId );
+        if ( $playerName eq $player->name ) {
+            my $approach = $league->approach;
+            $c->stash->{league}   = $leagueId;
+            $c->stash->{player}   = $playerName;
+            $c->stash->{id}       = $playerId;
+            $c->stash->{round}    = $round;
+            $c->stash->{approach} = $approach;
+            my $exercise;
+            if ( $approach eq "compwork" ) {
+                my $qns     = $work->correct($round);
+                my $correct = $qns->{$playerId};
+                my $someothers  = $work->opponents($round);
+                my $otherid = $someothers->{$playerId};
+		my $other;
+                if ( $otherid =~ m/bye|late|unpaired|transfer/i ) {
+		    $other = Nonentity->new( league => $league, id => $otherid,
+						name => $otherid );
 		}
+		else {
+                    $other = Player->new(
+                        league => $league,
+                        id     => $otherid
+                    );
+		}
+		my $othercorrect = $qns->{$otherid};
+		$exercise = {
+		    correct      => $correct,
+		    otherid      => $otherid,
+		    othername    => $other->name,
+		    othercorrect => $othercorrect
+		};
+	    }
+	    else {
+		my $session  = $work->week2session($round);
+		my $can      = $work->name2beancan( $round, $playerName );
+		my $members  = $work->beancans($session)->{$can};
+		my $merits   = $work->merits($round)->{$can};
+		my $demerits = $work->demerits($round)->{$can};
+		$exercise = {
+		    beancan  => $can,
+		    members  => $members,
+		    merits   => $merits,
+		    demerits => $demerits,
+		};
+	    }
+	    $c->stash->{exercise} = $exercise;
+	    $c->stash->{template} = 'rawclasswork.tt2';
 	}
+    }
 }
 
 =head2 demerits
@@ -184,5 +193,7 @@ This library is free software, you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =cut
+
+# vim: set ts=8 sts=4 sw=4 noet:
 
 1;
