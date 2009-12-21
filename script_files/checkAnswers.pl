3caa6cd0e76a3a811229c31287028f71542bb691
drbean <drbean@a817cad4-e040-dd11-9b0b-000bcdefe56b>
2009-12-21 09:13:42 +0000

    r2591@e_learning:  drbean | 2009-12-09 10:50:21 +0800  Pull answers from exam/response.yaml and from topic and form in cards.yaml and add up scores.

---
+++ script_files/checkAnswers.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use YAML qw/Bless Dump/;
+use Grades;
+
+my $answers = Grades::Script->new_with_options;
+my $id = $answers->league;
+my $exam = $answers->exam;
+
+my $league = League->new( id => $id );
+my $grades = Grades->new( league => $league );
+
+my $groups = $grades->examGroups( $exam );
+
+my $response;
+for my $group ( keys %$groups ) {
+	my $quiz = $grades->quiz( $exam, $group );
+	my $topic = $grades->topic($exam, $group);
+	my $form = $grades->form($exam, $group);
+	my ($codedvalue, $n);
+	for my $item ( @$quiz ) {
+		if ( $item->{option} ) {
+			my $option = $item->{option};
+			$codedvalue->[$n++] = { map {
+				$option->[$_] => $_ } 0..$#$option };
+		}
+		else { $codedvalue->[$n++] = { True => 1, False => 0 }; }
+	}
+	my $idsbyRole = $grades->idsbyRole( $exam, $group );
+	my $responses = $grades->responses( $exam, $group );
+	for my $id ( @$idsbyRole ) {
+		my $score = 0;
+		for my $n ( 0 .. $#$quiz ) {
+			my $myanswer = $responses->{$id}->{$n+1};
+			my $theanswer = $codedvalue->[$n]->{
+				$quiz->[$n]->{answer} };
+			unless ( defined $myanswer ) {
+				warn "${id}'s answer on question " . ($n+1) .
+					" in " . $topic . $form . " quiz?";
+				next;
+			}
+			unless ( defined $theanswer ) {
+				die "Right answer on question " . ($n+1) .
+					" in " . $topic . $form . " quiz?";
+			}
+			$score++ if $myanswer == $theanswer;
+		}
+		$response->{$group}->{$id} = $score;
+		$response->{$group}->{story} = $grades->topic( $exam, $group ) .
+				$grades->form( $exam, $group );
+	}
+	Bless( $response->{$group} )->keys([ @$idsbyRole, 'story' ]);
+}
+
+print Dump $response;
