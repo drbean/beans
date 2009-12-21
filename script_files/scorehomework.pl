60cd94e1aa8ccc4f40eea064bd0ac83db6841f66
drbean <drbean@a817cad4-e040-dd11-9b0b-000bcdefe56b>
2009-12-21 09:14:57 +0000

    r2614@e_learning:  drbean | 2009-12-19 17:37:07 +0800  MooseX options -x for exercise, -o for score of 1, -t for score of 2

---
+++ script_files/scorehomework.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl 
+
+use strict;
+use warnings;
+
+use Grades;
+
+my $script = Grades::Script->new_with_options;
+my $exercise = $script->exercise;
+my $two = $script->two;
+my $one = $script->one;
+
+use Cwd;
+use File::Basename;
+use YAML qw/LoadFile Dump/;
+
+my $cwd = getcwd;
+my $id = basename($cwd);
+
+my $l = League->new( id => $cwd );
+my $g = Grades->new( league => $l );
+my $members = $l->members;
+my %m = map { $_->{id} => $_ } @$members;
+
+my $standings = LoadFile '/var/www/cgi-bin/target/standings.yaml';
+
+my %p = map { $_ => $standings->{$id}->{$_}->{$exercise} } keys %m;
+$p{one} = $one;
+$p{two} = $two;
+$p{exercise} = $exercise;
+
+my %g = map { $_ => $p{$_} >= $two? 2: $p{$_} > $one? 1: 0 } keys %m;
+
+print Dump \%g;
+print Dump \%p;
