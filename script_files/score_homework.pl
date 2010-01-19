62c991b643a47c3ddffe444396f461c5938053d4
drbean <drbean@a817cad4-e040-dd11-9b0b-000bcdefe56b>
2010-01-19 10:23:35 +0000

    r3984@sac:  drbean | 2010-01-13 21:19:10 +0800  Use MooseX::Getopt rather than directory we are in. (This might have been mistake). Warn if non-member players, like Ya-hui 9631234, who transfered but whose results were still being stored in previous league.

--- script_files/score_homework.pl
+++ script_files/score_homework.pl
@@ -6,24 +6,25 @@ use warnings;
 use Grades;
 
 my $script = Grades::Script->new_with_options;
+my $id = $script->league;
 my $exercise = $script->exercise;
 my $two = $script->two;
 my $one = $script->one;
 
-use Cwd;
-use File::Basename;
 use YAML qw/LoadFile Dump/;
 
-my $cwd = getcwd;
-my $id = basename($cwd);
-
-my $l = League->new( id => $cwd );
+my $l = League->new( id => $id );
 my $g = Grades->new( league => $l );
 my $members = $l->members;
 my %m = map { $_->{id} => $_ } @$members;
 
 my $standings = LoadFile '/var/www/cgi-bin/target/standings.yaml';
 
+my $play = $standings->{$id};
+for my $player ( keys %$play ) {
+	warn "$player doing $exercise dictation not a member of $id league"
+		unless $m{$player};
+}
 my %p = map { $_ => $standings->{$id}->{$_}->{$exercise} } keys %m;
 $p{one} = $one;
 $p{two} = $two;
