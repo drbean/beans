0376ad500a184ffdc9fc0a7267de57a345fe8fae
drbean <drbean@a817cad4-e040-dd11-9b0b-000bcdefe56b>
2010-01-27 05:11:17 +0000

    r1838@momotaro:  greg | 2010-01-27 13:42:13 +0900  For exam listing, uri_for calculation in controller, adding of exam round in view. For raw, 'playerName' becomes 'name', roundreports takes report for each round, where 'jigsaw' is location of round data, of group, ids, topic, form, quiz, responses, scores, average, chinese

--- beans/branches/Grades/lib/beans/Controller/Exams.pm
+++ beans/branches/Grades/lib/beans/Controller/Exams.pm
@@ -29,41 +29,43 @@ Calculate exams score for one player using Moose exams script.
 =cut
 
 sub listing : Local {
-	my ($self, $c) = @_;
-	my $params = $c->request->params;
-	my $leagueId = $params->{league} || $c->request->args->[0];
-	my $playerId = $params->{id} || $c->request->args->[1];
-	my $player = $params->{player} || $c->request->args->[2];
-	my $league = League->new( id => $c->config->{leagues} . $leagueId );
-	my $work = Grades->new( league => $league );
-	if ( $league and $league->is_member($playerId) )
-	{
-		my $playerobj = Player->new(league => $league, id => $playerId);
-		if ( $player eq $playerobj->name ) {
-			$c->stash->{league} = $leagueId;
-			$c->stash->{player} = $player;
-			$c->stash->{id} = $playerId;
-			my ($weeks, @grades, %raw, $classwork);
-			my $exams = $work->examResults->{$playerId};
-			my $examGrade = $work->examGrade->{$playerId};
-			$examGrade = $work->sprintround($examGrade);
-			my $examPercent = $work->examPercent->{$playerId};
-			$examPercent = $work->sprintround($examPercent);
-			my $max = $work->examMax;
-			my $names = $work->examids;
-			my @exams = map { {	name => $names->[$_],
-						grade => $work->sprintround(
-							$exams->[$_]) . "/$max"
-					} } 0..$#$exams;
-			$c->stash->{exams} = \@exams;
-			$c->stash->{total} = $examGrade;
-			$c->stash->{percent} = $examPercent;
-			$c->stash->{weeks} = \@grades;
-			$c->stash->{template} = 'exams_listing.tt2';
-		}
-	}
+    my ( $self, $c ) = @_;
+    my $params   = $c->request->params;
+    my $leagueId = $params->{league} || $c->request->args->[0];
+    my $playerId = $params->{id} || $c->request->args->[1];
+    my $player   = $params->{player} || $c->request->args->[2];
+    my $league   = League->new( id => $c->config->{leagues} . $leagueId );
+    my $work     = Grades->new( league => $league );
+    if ( $league and $league->is_member($playerId) ) {
+        my $playerobj = Player->new( league => $league, id => $playerId );
+        if ( $player eq $playerobj->name ) {
+            $c->stash->{league} = $leagueId;
+            $c->stash->{player} = $player;
+            $c->stash->{id}     = $playerId;
+            my ( $weeks, @grades, %raw, $classwork );
+            my $exams     = $work->examResults->{$playerId};
+            my $examGrade = $work->examGrade->{$playerId};
+            $examGrade = $work->sprintround($examGrade);
+            my $examPercent = $work->examPercent->{$playerId};
+            $examPercent = $work->sprintround($examPercent);
+            my $max   = $work->examMax;
+            my $names = $work->examids;
+            my @exams = map {
+                {
+                    name  => $names->[$_],
+                    grade => $work->sprintround( $exams->[$_] ) . "/$max"
+                }
+            } 0 .. $#$exams;
+            $c->stash->{exams} = \@exams;
+            $c->stash->{raw} =
+		$c->uri_for( 'raw', $leagueId, $playerId, $player );
+            $c->stash->{total}    = $examGrade;
+            $c->stash->{percent}  = $examPercent;
+            $c->stash->{weeks}    = \@grades;
+            $c->stash->{template} = 'exams_listing.tt2';
+        }
+    }
 }
-
 
 =head2 raw
 
@@ -76,59 +78,56 @@ sub raw : Local {
     my $params     = $c->request->params;
     my $leagueId   = $params->{league} || $c->request->args->[0];
     my $playerId   = $params->{id} || $c->request->args->[1];
-    my $playerName = $params->{player} || $c->request->args->[2];
+    my $name = $params->{player} || $c->request->args->[2];
     my $exam       = $c->request->args->[3];
     my $league     = League->new( id => $c->config->{leagues} . $leagueId );
     my $work       = Grades->new( league => $league );
     if ( $league and $league->is_member($playerId) ) {
         my $player = Player->new( league => $league, id => $playerId );
-        if ( $playerName eq $player->name ) {
+        if ( $name eq $player->name ) {
             $c->stash->{league} = $leagueId;
-            $c->stash->{player} = $playerName;
+            $c->stash->{player} = $name;
             $c->stash->{id}     = $playerId;
             $c->stash->{examId} = $exam;
 	    my $rounds = $work->examrounds( $exam );
-            my ( @quiz, @responses, @ids, @topics, @forms, @scores, @chinese,
-		    @groups );
+            my @roundreports;
 	    do {
 		my $round = shift @$rounds;
-		my $task = $exam;
-		$task .= "/$round" if $round;
-		my $group = $work->name2jigsawGroup($task, $playerName )->[0];
-		push @groups, $group;
-		push @ids, $work->idsbyRole( $task, $group );
-		my $topic = $work->topic( $task, $group );
-		push @topics, $topic;
-		my $form = $work->form( $task, $group );
-		push @forms, $form;
+		my %report;
+		my $jigsaw = $work->examdirs;
+		$jigsaw .= "/$exam";
+		$jigsaw .= "/$round" if $round;
+		my $group = $work->name2jigsawGroup($jigsaw, $name )->[0];
+		$report{group} = $group;
+		$report{ids} = $work->idsbyRole( $jigsaw, $group );
+		my $topic = $work->topic( $jigsaw, $group );
+		$report{topic} = $topic;
+		my $form = $work->form( $jigsaw, $group );
+		$report{form} = $form;
 		try {
-			my $quiz = $work->quiz( $task, $group );
-			push @quiz, $quiz;
-			my $responses = $work->responses( $task, $group );
-			push @responses, $responses;
+			my $quiz = $work->quiz( $jigsaw, $group );
+			$report{quiz} = $quiz;
+			my $responses = $work->responses( $jigsaw, $group );
+			$report{responses} = $responses;
 		}
 		    catch {
     $c->stash->{status_msg} = "No information available on $topic $form quiz";
 		    };
-		my $member = $work->jigsawGroupMembers( $task, $group );
-		my $rawscores = $work->rawJigsawScores( $task, $group );
-		my $role = $work->id2jigsawGroupRole( $task, $group );
-		my @taskscores = map { $rawscores->{$_} }
+		my $member = $work->jigsawGroupMembers( $jigsaw, $group );
+		my $rawscores = $work->rawJigsawScores( $jigsaw, $group );
+		my $role = $work->id2jigsawGroupRole( $jigsaw, $group );
+		my @scores = map { $rawscores->{$_} }
 		    sort { $role->{$a} cmp $role->{$b} } keys %$rawscores;
-		push @scores, \@taskscores;
-		push @chinese, $work->jigsawDeduction( $exam, $group );
+		$report{scores} = \@scores;
+		$report{average} = sum( @scores ) / @scores;
+		$report{chinese} = $work->jigsawDeduction( $jigsaw, $group );
+		push @roundreports, \%report;
             } while @$rounds;
-            $c->stash->{ids} = \@ids;
-            $c->stash->{topic} = \@topics;
-            $c->stash->{form} = \@forms;
-            $c->stash->{quiz} = \@quiz;
-            $c->stash->{responses} = \@responses;
-            $c->stash->{scores} = \@scores;
-            $c->stash->{average} = sum( @scores ) / @scores;
-            $c->stash->{chinese} = \@chinese;
+	    $c->stash->{rounds} = \@roundreports;
             my $grade = $work->examResultHash->{$playerId}->{$exam};
             $c->stash->{grade}    = $grade;
-            $c->stash->{group}    = \@groups;
+            $c->stash->{raw} =
+              $c->uri_for( 'raw', $leagueId, $playerId, $name );
             $c->stash->{template} = 'rawexam.tt2';
         }
     }
