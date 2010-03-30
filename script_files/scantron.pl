66b8d22e36fc875ed2a29133e1667d689f6fef38
drbean <drbean@a817cad4-e040-dd11-9b0b-000bcdefe56b>
2010-03-30 14:31:36 +0000

    Use slices rather than for loops

--- script_files/scantron.pl
+++ script_files/scantron.pl
@@ -5,9 +5,10 @@ use warnings;
 
 use YAML qw/Bless Dump/;
 use Grades;
+use Cwd; use File::Basename;
 
 my $scantron = Grades::Script->new_with_options;
-my $id = $scantron->league;
+my $id = $scantron->league || basename( getcwd );
 my $exam = $scantron->exam;
 
 my $league = League->new( id => $id );
@@ -23,10 +24,10 @@ my $response;
 for my $group ( keys %$groups ) {
 	my $idsbyRole = $grades->idsbyRole( $exam, $group);
 	my $qn = $grades->qn( $exam, $group );
-	for my $id ( @$idsbyRole ) {
-		$response->{$group}->{$id} = { map { $_ => 0 } 1 .. $qn };
-		Bless( $response->{$group}->{$id} )->keys( [1..$qn] );
-	}
+	my $groupresponse = $response->{$group};
+	my %questions; @questions{1..$qn } = ( undef ) x $qn;
+	@$groupresponse{@$idsbyRole} = ( \%questions ) x @$idsbyRole;
+	Bless( $response->{$group}->{$_} )->keys( [1..$qn] ) for @$idsbyRole;
 	Bless( $response->{$group} )->keys( $idsbyRole );
 }
 print Dump $response;
