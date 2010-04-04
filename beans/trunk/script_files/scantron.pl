8bb4f8eb78eaa2ea43f5a579220363ff0021618e
drbean <drbean@a817cad4-e040-dd11-9b0b-000bcdefe56b>
2010-04-04 10:53:32 +0000

    perl-support POD entry copied and pasted from temp.pl. Worked well.

--- beans/trunk/script_files/scantron.pl
+++ beans/trunk/script_files/scantron.pl
@@ -1,5 +1,21 @@
 #!/usr/bin/perl
 
+# Created: 西元2010年04月04日 18時44分24秒
+# Last Edit: 2010  4月 04, 18時51分39秒
+# $Id$
+
+=head1 NAME
+
+scantron.pl - A YAML form to enter group's responses to jigsaw questions
+
+=head1 VERSION
+
+Version 0.02
+
+=cut
+
+our $VERSION = '0.02';
+
 use strict;
 use warnings;
 
@@ -7,9 +23,22 @@ use YAML qw/Bless Dump/;
 use Grades;
 use Cwd; use File::Basename;
 
+=head1 SYNOPSIS
+
+scantron.pl -l emile -r 3
+
+=cut
+
 my $scantron = Grades::Script->new_with_options;
 my $id = $scantron->league || basename( getcwd );
 my $exam = $scantron->round;
+
+
+=head1 DESCRIPTION
+
+Makes it easy to transfer from paper form in ttb/forms/compcomp/response.tex, to exams/3/response.yaml. Also use nn, nun autogroups (:do nn User, :do nun User) for one-touch data entry.
+
+=cut
 
 my $league = League->new( id => $id );
 my $grades = Grades->new( league => $league );
@@ -18,7 +47,6 @@ my $members = $league->members;
 
 my %members = map { $_->{name} => $_ } @$members;
 my $groups = $grades->jigsawGroups( $exam );
-
 
 my $response;
 for my $group ( keys %$groups ) {
