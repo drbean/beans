9cc4f50398b58b56c4cb4318e73522b732197551
drbean <drbean@a817cad4-e040-dd11-9b0b-000bcdefe56b>
2010-05-31 05:30:19 +0000

    New Classwork, Approaches classes. Order forms by topics to allow comparing responses while keying in.

--- beans/trunk/script_files/scantron.pl
+++ beans/trunk/script_files/scantron.pl
@@ -1,12 +1,12 @@
 #!/usr/bin/perl
 
 # Created: 西元2010年04月04日 18時44分24秒
-# Last Edit: 2010  4月 04, 21時14分18秒
+# Last Edit: 2010  5月 31, 13時30分26秒
 # $Id$
 
 =head1 NAME
 
-scantron.pl - A YAML form to enter group's responses to jigsaw questions
+scantron.pl - Generate .yaml file to enter group's responses to jigsaw questions
 
 =head1 VERSION
 
@@ -37,22 +37,27 @@ my $exam = $scantron->round;
 
 =head1 DESCRIPTION
 
-Makes it easy to transfer from paper form in ttb/forms/compcomp/response.tex, to exams/3/response.yaml. Also use nn, nun autogroups (:do nn User, :do nun User) for one-touch data entry.
+Makes it easy to transfer from paper subject/quiztopicform.tex quiz form, to exams/3/response.yaml. Also use nn, nun autogroups (:do nn User, :do nun User # see my vimrc) for one-touch data entry.
 
 =cut
 
 my $league = League->new( id => $id );
-my $grades = Grades->new( league => $league );
+my $approach = Approach->new( league => $league );
+my $classwork = Classwork->new( approach => $approach );
+my $grades = Grades->new( league => $league, classwork => $classwork );
 
 my $members = $league->members;
 
 my %members = map { $_->{name} => $_ } @$members;
 my $groups = $grades->jigsawGroups( $exam );
 
-my $response;
+my ($response, %formorder);
 for my $group ( keys %$groups ) {
 	my $idsbyRole = $grades->idsbyRole( $exam, $group);
 	warn "Only @$idsbyRole in $group," if not all { defined } @$idsbyRole;
+	my $form = $grades->topic( $exam, $group ) .
+			$grades->form( $exam, $group );
+	push @{ $formorder{$form} }, $group;
 	my $qn = $grades->qn( $exam, $group );
 	my $groupresponse = $response->{$group};
 	my %questions; @questions{1..$qn } = ( undef ) x $qn;
@@ -61,6 +66,8 @@ for my $group ( keys %$groups ) {
 	Bless( $response->{$group}->{$_} )->keys( [1..$qn] ) for @$idsbyRole;
 	Bless( $response->{$group} )->keys( $idsbyRole );
 }
+my @formorders = values %formorder;
+Bless( $response )->keys([ map { sort @$_ } @formorders ]);
 $YAML::UseAliases = 0;
 print Dump $response;
 
