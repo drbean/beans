814062a5a82fb52ec83f8c42f028678f06d8e5e1
drbean <drbean@a817cad4-e040-dd11-9b0b-000bcdefe56b>
2013-06-06 07:56:33 +0000

    r7495@sac:  drbean | 2013-03-24 17:02:06 +0800  Back out non-save g2.yaml, spacing, indenting of table print out

--- script_files/print_grades.pl
+++ script_files/print_grades.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl 
 
 # Created: 03/21/2013 10:08:14 PM
-# Last Edit: 2013 Mar 22, 09:21:54 AM
+# Last Edit: 2013 Mar 24, 04:49:33 PM
 # $Id$
 
 =head1 NAME
@@ -54,7 +54,7 @@ my %classwork = map { $_ => $g->sprintround( $classwork->{$_} ) } keys %$classwo
 my $comp = Compcomp->new({ league => $l });
 my $ex1 = $l->inspect("$dirs/$dir/exam/$session/g1.yaml");
 my $ex2 = $comp->points($session);
-# $l->save("$dirs/$dir/exam/$session/g2.yaml", $ex2);
+$l->save("$dirs/$dir/exam/$session/g2.yaml", $ex2);
 $ex2 = $l->inspect("$dirs/$dir/exam/$session/g2.yaml");
 my %exams = map { $_ => ( $ex1->{$_} + $ex2->{$_} ) / 2 } keys %m;
 $l->save("$dirs/$dir/exam/$session/g.yaml", \%exams);
@@ -69,11 +69,11 @@ my @grades = $l->id . " " . $l->name . " " . $l->field . " Grades\n" .
 "Classwork: " . $weights->{classwork} . "\n" .
 "Homework: " . $weights->{homework} . "\n" .
 "Exams: " . $weights->{exams} . "\n" .
-"Name\tId\tClasswork\tHomework\Exams\Grade\n";
+"Name\tId\t   Classwork    Homework\tExams\tGrade\n";
 my @ids = sort keys %m;
 for my $id ( @ids ) {
 	push @grades,
-"$m{$id}->{name}\t$id\t$classwork{$id}\t$hw{$id}\t$ex{$id}\t$grade->{$id}\n";
+"$m{$id}->{name}\t$id\t\t$classwork{$id}\t$hw{$id}\t$ex{$id}\t$grade->{$id}\n";
 }
 
 
