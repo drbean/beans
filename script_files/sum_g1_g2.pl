c1abe33b892e22039e4d6af037efcbb1e898653c
drbean <drbean@a817cad4-e040-dd11-9b0b-000bcdefe56b>
2014-03-25 00:39:57 +0000

    "my" variable $g2 masks earlier declaration in same scope at /usr/local/bin/sum_g1_g2.pl line 52. LoadFile from DumpFiled $g2

--- script_files/sum_g1_g2.pl
+++ script_files/sum_g1_g2.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl 
 
 # Created: 04/28/2013 04:26:17 PM
-# Last Edit: 2013 Dec 15, 09:34:18 PM
+# Last Edit: 2014 Mar 25, 08:31:30 AM
 # $Id$
 
 =head1 NAME
@@ -46,11 +46,11 @@ my $leagues = $league->leagues;
 my $g1 = LoadFile "$leagues/$id/exam/$exam/g1.yaml" or die "g1.yaml?";
 my $g2 = $co->points($exam);
 DumpFile "$leagues/$id/exam/$exam/g2.yaml", $g2 or die "g2.yaml?";
-my $g2 = $league->inspect("$leagues/$id/exam/$exam/g2.yaml");
+my $g2_again = $league->inspect("$leagues/$id/exam/$exam/g2.yaml");
 my %g = map {
 		die "Player $_ missing from g1.yaml" if not defined $g1->{$_};
-		die "Player $_ missing from g2.yaml" if not defined $g2->{$_};
-		$_ => ( $g1->{$_} + $g2->{$_} ) / 2
+		die "Player $_ missing from g2.yaml" if not defined $g2_again->{$_};
+		$_ => ( $g1->{$_} + $g2_again->{$_} ) / 2
 	    } keys %m;
 
 
