a0407062730643c73c5fc3fe32207c924f5e55de
drbean <drbean@a817cad4-e040-dd11-9b0b-000bcdefe56b>
2014-06-02 08:29:38 +0000

    member_id_list

---
+++ script_files/print_ids.pl
@@ -0,0 +1,69 @@
+#!/usr/bin/perl 
+
+# Created: 西元2014年06月02日 15時04分40秒
+# Last Edit: 2014  6月 02, 15時49分27秒
+# $Id$
+
+=head1 NAME
+
+print_members.pl - \n separated list of members of league
+
+=cut
+
+use strict;
+use warnings;
+
+use YAML qw/LoadFile Dump/;
+use IO::All;
+use Cwd; use File::Basename;
+
+=head1 SYNOPSIS
+
+print_ids.pl -l GL00019 > members.txt
+
+=cut
+
+# use Grades;
+
+# my $script = Grades::Script->new_with_options( league => basename(getcwd) );
+# pod2usage(1) if $script->help;
+# pod2usage(-exitstatus => 0, -verbose => 2) if $script->man;
+# my $leagueId = $script->league;
+# $leagueId = basename( getcwd ) if $leagueId eq '.';
+# my $l = League->new( id => $leagueId );
+# my $members = $l->members;
+# my @ids = sort map { $_->{id} } @$members;
+
+my $leagueId = basename( getcwd );
+my $y = LoadFile "/home/drbean/022/$leagueId/league.yaml";
+my $members = $y->{member};
+my @ids = sort map { $_->{id} } @$members;
+$, = "\n";
+print STDOUT @ids;
+
+=head1 DESCRIPTION
+
+Use in vim to get list of ids, eg :r! print_ids.pl
+
+=cut
+
+
+
+=head1 AUTHOR
+
+Dr Bean C<< <drbean at cpan, then a dot, (.), and org> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2014 Dr Bean, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+# End of print_members.pl
+
+# vim: set ts=8 sts=4 sw=4 noet:
+
+
