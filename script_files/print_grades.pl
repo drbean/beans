e1982b70c2b38d926acd715d92aad6fbeb17edd4
drbean <drbean@a817cad4-e040-dd11-9b0b-000bcdefe56b>
2015-07-07 05:41:43 +0000

    print_grades.pl -x curve -o 60 -w 80 -t 100 > grades.txt. A gradesheet, with grades curved from low, through median to high if exercise (-x) is "curve".

--- script_files/print_grades.pl
+++ script_files/print_grades.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl 
 
 # Created: 03/21/2013 10:08:14 PM
-# Last Edit: 2015  2月 24, 13時11分56秒
+# Last Edit: 2015  7月 07, 13時39分26秒
 # $Id$
 
 =head1 NAME
@@ -18,7 +18,7 @@ use Cwd;
 
 =head1 SYNOPSIS
 
-print_grades.pl > grades.txt
+print_grades.pl -x curve -o 60 -w 80 -t 100 > grades.txt
 
 =cut
 
@@ -27,6 +27,7 @@ my $session = 1;
 my $dirs = '/home/drbean/032';
 
 (my $dir = getcwd) =~ s/^.*\/([^\/]*)$/$1/;
+
 use Grades;
 my $l = League->new( leagues => $dirs, id => $dir );
 my $g = Grades->new({ league => $l });
@@ -34,9 +35,16 @@ my %m = map { $_->{id} => $_ } @{ $l->members };
 my $approach = $l->approach;
 my $c = $g->classwork;
 
+my $script = Grades::Script->new_with_options;
+my $low = $script->one || 60;
+my $high = $script->two || 100;
+my $median = $script->weights || 80;
+my $curving = $script->exercise;
+
 =head1 DESCRIPTION
 
-A gradesheet
+A gradesheet, with grades curved from low, through median to high if exercise (-x) is "curve".
+
 
 =cut
 
@@ -48,14 +56,27 @@ my %classwork = map { $_ => $g->sprintround( $classwork->{$_} ) } keys %$classwo
 my $ex = $g->examPercent;
 my %ex = map { $_ => $g->sprintround( $ex->{$_} ) } keys %$ex;
 
-my $grade = $g->grades;
+my $grade;
+if ( defined $curving and $curving and $curving eq "curve" ) {
+    $grade = $g->curve( $low, $median, $high );
+}
+else {
+    $grade = $g->grades;
+}
 
 my $weights = $g->weights;
 my @grades = $l->id . " " . $l->name . " " . $l->field . " Grades\n" .
 "Classwork: " . $weights->{classwork} . "\n" .
 "Homework: " . $weights->{homework} . "\n" .
-"Exams: " . $weights->{exams} . "\n" .
-"Name\tId\t   Classwork    Homework\tExams\tGrade\n";
+"Exams: " . $weights->{exams} . "\n";
+
+push @grades, 
+"Curving:\n" .
+"  Low: " . $low . "\n" .
+"  Median: " . $median . "\n" .
+"  High: " . $high . "\n" .
+"Name\tId\t   Classwork    Homework\tExams\tGrade\n" if $curving eq "curve";
+
 my @ids = sort keys %m;
 for my $id ( @ids ) {
 	push @grades,
@@ -65,6 +86,9 @@ for my $id ( @ids ) {
 
 print(@grades);
 
+warn "Classwork weeks: @{ $c->all_events } \n";
+warn "Homework weeks: @{ $g->rounds } \n";
+warn "Exams: @{ $g->examids } \n";
 
 =head1 AUTHOR
 
