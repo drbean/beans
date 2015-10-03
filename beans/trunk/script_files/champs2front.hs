17192342c3a9dec74e91b2263acdca57f836be49
drbean <drbean@a817cad4-e040-dd11-9b0b-000bcdefe56b>
2015-10-03 04:03:35 +0000

    comment_out:test_cwk.ambiguous_types

--- beans/trunk/script_files/champs2front.hs
+++ beans/trunk/script_files/champs2front.hs
@@ -80,13 +80,13 @@ r2a (R int) = (A int)
 a2int (A int) = int
 r2int (R int) = int
 
-test_cwk :: Value
-test_cwk = object [ "topic" .= "lerman",
-	"eleven'" .= object [ "tardy" .= [], "absent" .= [], "merits" .= 0, "rs" .= [ 0 1 ] ],
-	"twelve'" .= object [ "tardy" .= [], "absent" .= [], "merits" .= 0, "rs" .= [ 0 1 ] ],
-	"twentyone'" .= object [ "tardy" .= [], "absent" .= [], "merits" .= 0, "rs" .= [ 0 1 ] ],
-	"quiz" .= [ (object [ "q" .= "What is the answer", "o" .= [ "True", "False" ], "a" .= 0 ]), (object [ "q" .= "What is the answer", "o" .= [ "True", "False" ], "a" .= 0 ]) ]
-	]
+--test_cwk :: Value
+--test_cwk = object [ "topic" .= "lerman",
+--	"eleven'" .= object [ "tardy" .= [], "absent" .= [], "merits" .= 0, "rs" .= [ 0 1 ] ],
+--	"twelve'" .= object [ "tardy" .= [], "absent" .= [], "merits" .= 0, "rs" .= [ 0 1 ] ],
+--	"twentyone'" .= object [ "tardy" .= [], "absent" .= [], "merits" .= 0, "rs" .= [ 0 1 ] ],
+--	"quiz" .= [ (object [ "q" .= "What is the answer", "o" .= [ "True", "False" ], "a" .= 0 ]), (object [ "q" .= "What is the answer", "o" .= [ "True", "False" ], "a" .= 0 ]) ]
+--	]
 
 main = do
 	y <- Data.Yaml.decodeFile s :: IO (Maybe Session)
